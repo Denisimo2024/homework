a2debb3e2cd27665efc392b3722c19a097cd0cbd
Denisimo2024 <2132813@gmail.com>
2024-10-30 19:33:56 +0500

    start

---
+++ main.py
@@ -0,0 +1,20 @@
+class User:
+    def __int__(self, ID, name, access_level ):
+        self.ID = ID
+        self.name = name
+        self.access_level = access_level
+
+
+
+
+
+
+class Admin:
+    def super().super()
+
+
+    def __add_user:
+
+
+    def __remove_user:
+
